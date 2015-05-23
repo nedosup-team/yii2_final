9c961987e33a19f49ef3c0992212d7fb52c0c1fc
Dmytro Korelsky <korelsky.d@gmail.com>
2015-05-23 12:41:07 +0300

    Update admin user password to 123456

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -29,10 +29,10 @@ class m130524_201442_init extends Migration
         $this->insert('{{%user}}', [
             'id' => 1,
             'username' => "admin",
-            'auth_key' => "iM53eQsUrioK-kGF7-kfknzYHaynrzsa",
-            'password_hash' => '$2y$13$ptJMksESBfxZFz7XE4gZCey/JREZlKK0HDxrDuTOPnkkh.jjX3fNO',
+            'auth_key' => "GQqkcLP51dkHf1o1uacuZrPNXetvE2cf",
+            'password_hash' => '$2y$13$vHGftYBCeKS2MhKV1y/BrOpg9NQnWeLiNbLZtouYJpv1.5w1u5kOG',
             'password_reset_token' => NULL,
-            'email' => "helper@yii2.local",
+            'email' => "admin@yii2.final",
             'status' => 10,
             'created_at' => time(),
             'updated_at' => time(),
